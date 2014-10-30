04adcbfccc9d78932cc00a57112211905fba38f0
wulifu2hao <wulifu20hao@gmail.com>
2014-10-30 17:37:12 +0800

    add loading and displaying project names when loading the index page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,29 +23,39 @@ window.Playground = {
     }
 };
 
-    // var url = '/api/programs';
+    
 
-    // $.ajax({
-    //     type: 'GET',
-    //     url: url,
-    //     success: function(data) {
-    //         if (data.message == "success") {
-    //             var body = "";
-    //             for (var i = 0; i < data.names.length; i++) {
-    //                 var item = "<li><a href="#">"+data.names[i]"</a></li>";
-    //                 body += item;
-    //             };
-    //             $("#projectList").val(body);
-    //         } 
-    //     },
-    //     error: function(err){
-    //      }
-    // });
+    
 
 
 
 $(document).ready(function () {
     'use strict';
     Playground.init();
-    Backbone.history.start();    
+    Backbone.history.start();  
+
+    var url = '/api/programs';
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function(data) {
+            if (data.message == "success") {
+                var ul = document.getElementById('projectList');
+                  if (ul) {
+                    while (ul.firstChild) {
+                      ul.removeChild(ul.firstChild);
+                    }
+                  }
+                for (var i = 0; i < data.names.length; i++) {
+                    var item = "<li><a href='#'>"+data.names[i]+"</a></li>";
+                    $("#projectList").append(item);
+                };
+            } 
+        },
+        error: function(err){
+
+        }
+    });
+
 });
