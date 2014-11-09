8abb35cf11c4d8df21dca03ae3e3f7f52972915e
BaiXF <baixuefeng1127@gmail.com>
2014-11-10 00:59:55 +0800

    player.js
    fit for multi sprites

--- app/scripts/views/player.js
+++ app/scripts/views/player.js
@@ -11,7 +11,7 @@ Playground.Views = Playground.Views || {};
 
         el: '#player',
         
-        current_status : {},                  // draw this status in current frame
+        current_status : [],                  // draw this status in current frame
         func_name : '',                       // current function executing
         commands_list : [],                   // list of functions need to be executed
         index : 0,                              
@@ -38,8 +38,9 @@ Playground.Views = Playground.Views || {};
                 e.preventDefault();
                 window.location = (window.location + 'auth/google');
             });
-         
-            this.current_status = {              // init status
+            
+            for (var n=0; n<this.model.numOfSprite; n++)
+            this.current_status[n] = {              // init status
                         xPos: this.model.spriteModel[0].get('xPos'),
                         yPos: this.model.spriteModel[0].get('yPos'),
                         angle: this.model.spriteModel[0].get('angle'),
@@ -57,10 +58,10 @@ Playground.Views = Playground.Views || {};
                 // console.log("mousedown");
                 var mouseX = parseInt(event.clientX - event.target.offsetLeft);
                 var mouseY = parseInt(event.clientY - event.target.offsetTop);
-                var x = that.current_status.xPos;
-                var y = that.current_status.yPos;
-                var w = that.current_status.width;
-                var h = that.current_status.height;
+                var x = that.current_status[0].xPos;
+                var y = that.current_status[0].yPos;
+                var w = that.current_status[0].width;
+                var h = that.current_status[0].height;
                 if((mouseX > (x )) && (mouseX < (x + w)) && (mouseY > (y )) && (mouseY < (y+h))){
                     that.isDragging = true;
                 }
@@ -74,13 +75,13 @@ Playground.Views = Playground.Views || {};
             $('#player_canvas').mousemove(function(event){
                 var mouseX = parseInt(event.clientX - event.target.offsetLeft);
                 var mouseY = parseInt(event.clientY - event.target.offsetTop);
-                var x = that.current_status.xPos;
-                var y = that.current_status.yPos;
-                var w = that.current_status.width;
-                var h = that.current_status.height;
+                var x = that.current_status[0].xPos;
+                var y = that.current_status[0].yPos;
+                var w = that.current_status[0].width;
+                var h = that.current_status[0].height;
                 if(that.isDragging){
-                    that.current_status.xPos = mouseX - w/2;
-                    that.current_status.yPos = mouseY - h/2;
+                    that.current_status[0].xPos = mouseX - w/2;
+                    that.current_status[0].yPos = mouseY - h/2;
                     that.draw();
                 }
             });
@@ -90,22 +91,22 @@ Playground.Views = Playground.Views || {};
             //     switch (evt.keyCode) {
             //     case 87:  /* w was pressed */
             //     console.log("up");
-            //     that.current_status.yPos -= 5;
+            //     that.current_status[0].yPos -= 5;
             //     that.draw();
             //     break;
             //     case 83:  /* s was pressed */
             //     console.log("down");
-            //     that.current_status.yPos += 5;
+            //     that.current_status[0].yPos += 5;
             //     that.draw();
             //     break;
             //     case 65:  /* a was pressed */
             //     console.log("left");
-            //     that.current_status.xPos -= 5;
+            //     that.current_status[0].xPos -= 5;
             //     that.draw();
             //     break;
             //     case 68:  /* d was pressed */
             //     console.log("right");
-            //     that.current_status.xPos += 5;
+            //     that.current_status[0].xPos += 5;
             //     that.draw();
             //     break;
             //     }
@@ -224,7 +225,7 @@ Playground.Views = Playground.Views || {};
                         else{
                             ind += command.para[1];
                             // that.executeFunctions(id+1+command.para[1], command.para[2]);
-                        }∂
+                        }
                 }
                 this.executeCommand(ind, command);
             }
@@ -235,24 +236,24 @@ Playground.Views = Playground.Views || {};
                     
                     case "setXPos":        
                         // this.ctx.rotate(this.getValueOf(command.para[0])*Math.PI/180);
-                        // this.current_status.angle = this.getValueOf(command.para[0])*Math.PI/180;              
-                        this.current_status.xPos = this.getValueOf(command.para[0]);
+                        // this.current_status[0].angle = this.getValueOf(command.para[0])*Math.PI/180;              
+                        this.current_status[0].xPos = this.getValueOf(command.para[0]);
                         this.draw();
                         break;
 
                     case "setYPos":
                         console.log("setYPos");
-                        this.current_status.yPos = this.getValueOf(command.para[0]);
+                        this.current_status[0].yPos = this.getValueOf(command.para[0]);
                         this.draw();
                         break;
 
                     case "show":
-                        this.current_status.isShown = true;
+                        this.current_status[0].isShown = true;
                         this.draw();
                         break;
 
                     case "hide":
-                        this.current_status.isShown = false;
+                        this.current_status[0].isShown = false;
                         this.draw();
                         break;
 
@@ -261,21 +262,21 @@ Playground.Views = Playground.Views || {};
                         //move in current facing direction
                         var step = 0;
                         while (step < this.getValueOf(command.para[0])){
-                            // this.current_status.xPos += Math.cos()*this.current_status.angle;
-                            // this.current_status.yPos += Math.sin()*this.current_status.angle;
-                            this.current_status.xPos++;
-                            console.log("current pos", this.current_status.xPos);
+                            // this.current_status[0].xPos += Math.cos()*this.current_status[0].angle;
+                            // this.current_status[0].yPos += Math.sin()*this.current_status[0].angle;
+                            this.current_status[0].xPos++;
+                            console.log("current pos", this.current_status[0].xPos);
                             this.draw();
                             step++;
                         }
                         break;
 
                     case "changeCostume":
-                        if(this.costume<(this.current_status.costumes.length-1)){
+                        if(this.costume<(this.current_status[0].costumes.length-1)){
                             console.log("costume change to next");
                             this.costume ++;
                         }
-                        else if(this.costume===(this.current_status.costumes.length-1)){
+                        else if(this.costume===(this.current_status[0].costumes.length-1)){
                             console.log("costume change back to 0");
                             this.costume = 0;
                         }
@@ -383,7 +384,7 @@ Playground.Views = Playground.Views || {};
                         //parameters: angle
                         this.clearCanvas();
                         this.ctx.rotate(this.getValueOf(command.para[0])*Math.PI/180);
-                        this.current_status.angle = this.getValueOf(command.para[0])*Math.PI/180;
+                        this.current_status[0].angle = this.getValueOf(command.para[0])*Math.PI/180;
                         this.draw();
                         break;
 
@@ -487,8 +488,8 @@ Playground.Views = Playground.Views || {};
                 return x;
             }
             switch (x){
-                case "xPos": return this.current_status.xPos;
-                case "yPos": return this.current_status.yPos;
+                case "xPos": return this.current_status[0].xPos;
+                case "yPos": return this.current_status[0].yPos;
                 default:
             }
             for(var k in this.variables) {
@@ -517,12 +518,12 @@ Playground.Views = Playground.Views || {};
             }
 
             i=0;
-            while(i<this.current_status.costumes.length){
+            while(i<this.current_status[0].costumes.length){
                 this.spriteImgs[i] = new Image();
                 this.spriteImgs[i].onload = function(){
                     console.log("load sprites imgs");
                 }
-                this.spriteImgs[i].src = this.current_status.costumes[i];
+                this.spriteImgs[i].src = this.current_status[0].costumes[i];
                 i++;
             }
         },
@@ -533,17 +534,17 @@ Playground.Views = Playground.Views || {};
 
         drawCharacter: function(){
             var that = this;
-            var shown = this.current_status.isShown;
-            if(that.current_status.isShown){
-                     that.ctx.drawImage(this.spriteImgs[this.costume],that.current_status.xPos, that.current_status.yPos, that.current_status.width, that.current_status.height); //character.width, character.height);     // draw costume if status isShown is true.
+            var shown = this.current_status[0].isShown;
+            if(that.current_status[0].isShown){
+                     that.ctx.drawImage(this.spriteImgs[this.costume],that.current_status[0].xPos, that.current_status[0].yPos, that.current_status[0].width, that.current_status[0].height); //character.width, character.height);     // draw costume if status isShown is true.
                  }     
         },
 
         drawCharacterResize: function(x, y){
             var that = this;
-            var shown = this.current_status.isShown;
-            if(that.current_status.isShown){
-                     that.ctx.drawImage(this.spriteImgs[this.costume],that.current_status.xPos, that.current_status.yPos, x*80, y*150); //character.width, character.height);     // draw costume if status isShown is true.
+            var shown = this.current_status[0].isShown;
+            if(that.current_status[0].isShown){
+                     that.ctx.drawImage(this.spriteImgs[this.costume],that.current_status[0].xPos, that.current_status[0].yPos, x*80, y*150); //character.width, character.height);     // draw costume if status isShown is true.
                  }   
         },
 
